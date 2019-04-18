7a66eaf276b72ee68eea5ea08f251bca2a5cf610
J-Chris-Garcia <chrisg905@gmail.com>
2019-04-18 11:54:15 -0500

    added code for 1a and dictionary for 2

--- project2.py
+++ project2.py
@@ -1,18 +1,23 @@
-#import csv module
 import csv 
+from datetime import datetime
 
-d = dict()
-# reading csv file
+moviespermonth = {} #use this for question 2
+date_format = '%m/%d/%Y'
+
 with open ('2016_movie_data.csv', 'rt', encoding = 'ISO-8859-1') as csvfile:
-
-    csvreader = csv.DictReader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader) #skips first line
     
     for row in csvreader:
-        for key, value in row.items():
-            if d.get(key) is None:
-                d[key] = [value]
-            else:
-                d[key].append(value)
-   
-print(d)
-print('keys:', d.keys())
+        datetime_obj = datetime.strptime(row[1], date_format) #line 12/13 get the month from the date
+        month = datetime_obj.strftime("%B")
+        
+        if month in moviespermonth.keys():
+            moviespermonth[month] += 1
+        else:
+            moviespermonth[month] = 1
+
+        
+totalmovies = sum(moviespermonth.values())
+print(f"The total number of movies released: {totalmovies}")    
+print(moviespermonth)
